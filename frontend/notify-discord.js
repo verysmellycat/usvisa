7d830ead0b04e4bea891521fcd74fcb3fddbc292
hannnie2 <hannnie2@gmail.com>
2024-01-22 22:19:06 -0800

    updated favicon, added deployment notification

---
+++ frontend/notify-discord.js
@@ -0,0 +1,33 @@
+const https = require("https");
+
+const webhookURL = process.env.DISCORD_WEBHOOK_URL;
+const data = JSON.stringify({
+  content: "New deployment is live!",
+});
+
+const url = new URL(webhookURL);
+
+const options = {
+  hostname: url.hostname,
+  path: url.pathname,
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length,
+  },
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
