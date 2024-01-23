84e04e883b5908952b6a154484697473b2bfc693
usvisa.lol <you@example.com>
2024-01-23 09:37:41 -0800

    qr.jpg is case sensitive, should be lowercase

--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Payment = () => {
   return (
     <div className="flex flex-col items-center sm:flex-row sm:justify-center gap-y-3">
-      <img src="/QR.jpg" alt="QR payment" className="min-w-60" />
+      <img src="/qr.jpg" alt="QR payment" className="min-w-60" />
       <stripe-buy-button
         buy-button-id="buy_btn_1OXwo3EOWaPf9JJTBUlJ0KKx"
         publishable-key="pk_live_51OXSq1EOWaPf9JJTifoe9pk1hZhDCAGH278lptO9nSdpAdXlV2K0HkVjmMRAzGv7vgUPTpeJ1Pu2YOH0FZejC13t00h39KPe67"
