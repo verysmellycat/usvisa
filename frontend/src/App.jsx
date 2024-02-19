ecad55434183d1e3f59524851690c135407dbb1e
usvisa.lol <you@example.com>
2024-02-18 22:23:13 -0800

    Revert "remove notification"
    This reverts commit 733726bad27fe0f5d09b64f5be6a493f6dc43bdb.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,6 +49,12 @@ function App() {
   const { t } = useTranslation();
   return (
     <BrowserRouter>
+      <div className="flex items-center justify-center gap-x-2 w-full sticky top-0 z-30 bg-white border-b py-3">
+        <div className="shrink-0">
+          <AiFillNotification />
+        </div>
+        <p className="font-medium text-center">{t("notification")}</p>
+      </div>
       <div className="container mx-auto">
         <NavBar />
         <MainContent />
