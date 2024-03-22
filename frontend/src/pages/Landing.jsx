9ce6e047b76c294af3b523cfb694df673610d305
Eric N <hannnie2@gmail.com>
2024-03-21 18:50:19 -0700

    minor fix

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -19,99 +19,97 @@ export default function Landing() {
       transition={{ duration: 0.6, ease: "easeOut" }}
     >
       <div className="container mx-auto flex min-h-screen flex-col gap-y-6 pb-12">
-        <div className="flex flex-col gap-y-6 py-6 md:h-screen lg:gap-y-3">
-          <div className="flex grow flex-col justify-center gap-y-6">
-            <div className="text-center">
-              <p className="text-xl font-semibold text-fuchsia-700 lg:text-3xl xl:text-4xl">
-                usvisa.lol
-              </p>
-              <h1 className="text-6xl font-bold lg:text-7xl xl:text-8xl">
-                Visa made easier than ever
-              </h1>
-            </div>
-            <p className="text-center text-lg text-foreground-500">
-              Blazing fast, optimized system with multi-country support,
-              featuring automatic slot monitoring, rescheduling and real-time
-              notification. It does it all for you.
+        <div className="flex h-[600px] flex-col justify-center gap-y-6">
+          <div className="text-center">
+            <p className="text-xl font-semibold text-fuchsia-700 lg:text-3xl xl:text-4xl">
+              usvisa.lol
             </p>
-            <Button
-              onClick={() => scrollToBottom()}
-              className="max-w-[150px] self-center bg-foreground text-background"
-              size="lg"
-            >
-              Get started
-            </Button>
+            <h1 className="text-6xl font-bold lg:text-7xl xl:text-8xl">
+              Visa made easier than ever
+            </h1>
           </div>
-          <div className="flex flex-col gap-y-6">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-6 md:grid-cols-7">
-              <div className="space-y-3 border border-foreground px-3 py-3 md:col-span-4 md:py-6">
-                <div className="space-y-1.5">
-                  <h2 className="text-center text-lg font-semibold lg:text-2xl">
-                    Regular
-                  </h2>
-                  <p className="text-center text-base text-foreground-500 lg:text-lg">
-                    Our standard mode. Best for average use cases and suitable
-                    for most users.
-                  </p>
-                </div>
-                <div className="grid grid-cols-8 text-base lg:text-xl">
-                  <div className="col-span-2 space-y-2 font-medium">
-                    <p className="invisible text-center">placeholder</p>
-                    <p className="text-start">New user</p>
-                    <p className="text-start">Returning user</p>
-                  </div>
-                  <div className="col-span-3 space-y-2 text-center font-medium">
-                    <p>Europe</p>
-                    <p className="text-cyan-500"> CA$ 19.99</p>
-                    <p className="text-cyan-500"> CA$ 49.99</p>
-                  </div>
-                  <div className="col-span-3 space-y-2 text-center font-medium">
-                    <p>America</p>
-                    <p className="text-cyan-500">CA$ 49.99</p>
-                    <p className="text-cyan-500">CA$ 79.99</p>
-                  </div>
-                </div>
-                <p>
-                  <span className="text-red-500">*</span> per person price
+          <p className="text-center text-lg text-foreground-500">
+            Blazing fast, optimized system with multi-country support, featuring
+            automatic slot monitoring, rescheduling and real-time notification.
+            It does it all for you.
+          </p>
+          <Button
+            onClick={() => scrollToBottom()}
+            className="max-w-[150px] self-center bg-foreground text-background"
+            size="lg"
+          >
+            Get started
+          </Button>
+        </div>
+        <div className="flex flex-col gap-y-6">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-6 md:grid-cols-7">
+            <div className="space-y-3 border border-foreground px-3 py-3 md:col-span-4 md:py-6">
+              <div className="space-y-1.5">
+                <h2 className="text-center text-lg font-semibold lg:text-2xl">
+                  Regular
+                </h2>
+                <p className="text-center text-base text-foreground-500 lg:text-lg">
+                  Our standard mode. Best for average use cases and suitable for
+                  most users.
                 </p>
               </div>
-              <div className="flex flex-col gap-y-3 border border-foreground px-3 py-3 md:col-span-3 md:py-6">
-                <div className="space-y-1.5">
-                  <h2 className="text-center text-lg font-semibold lg:text-2xl">
-                    Pro
-                  </h2>
-                  <p className="text-center text-base text-foreground-500 lg:text-lg">
-                    Recommended only for those with emergency situations.
-                  </p>
+              <div className="grid grid-cols-8 text-base lg:text-xl">
+                <div className="col-span-2 space-y-2 font-medium">
+                  <p className="invisible text-center">placeholder</p>
+                  <p className="text-start">New user</p>
+                  <p className="text-start">Returning user</p>
                 </div>
-                <div className="flex grow flex-col justify-center gap-y-2 text-center text-base font-medium lg:text-xl">
-                  <p className="text-cyan-500">Varied price</p>
-                  <p className="lg:text-lg">
-                    Send an email to support@usvisa.lol for inquires
-                  </p>
+                <div className="col-span-3 space-y-2 text-center font-medium">
+                  <p>Europe</p>
+                  <p className="text-cyan-500"> CA$ 19.99</p>
+                  <p className="text-cyan-500"> CA$ 49.99</p>
+                </div>
+                <div className="col-span-3 space-y-2 text-center font-medium">
+                  <p>America</p>
+                  <p className="text-cyan-500">CA$ 49.99</p>
+                  <p className="text-cyan-500">CA$ 79.99</p>
                 </div>
               </div>
+              <p>
+                <span className="text-red-500">*</span> per person price
+              </p>
             </div>
-            <div className="space-y-2 text-sm text-foreground-500 lg:text-base">
-              <div className="space-y-1.5 text-foreground">
-                <p>You're eligible for a 50% refund if you:</p>
-                <p>1. Post on Xiaohongshu when you start using our website.</p>
-                <p>
-                  2. Collect at least price/2 likes and bookmarks (that's 10
-                  each if you paid $20).
+            <div className="flex flex-col gap-y-3 border border-foreground px-3 py-3 md:col-span-3 md:py-6">
+              <div className="space-y-1.5">
+                <h2 className="text-center text-lg font-semibold lg:text-2xl">
+                  Pro
+                </h2>
+                <p className="text-center text-base text-foreground-500 lg:text-lg">
+                  Recommended only for those with emergency situations.
                 </p>
               </div>
+              <div className="flex grow flex-col justify-center gap-y-2 text-center text-base font-medium lg:text-xl">
+                <p className="text-cyan-500">Varied price</p>
+                <p className="lg:text-lg">
+                  Send an email to support@usvisa.lol for inquires
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="space-y-2 text-sm text-foreground-500 lg:text-base">
+            <div className="space-y-1.5 text-foreground">
+              <p>You're eligible for a 50% refund if you:</p>
+              <p>1. Post on Xiaohongshu when you start using our website.</p>
               <p>
-                Pricing is determined by the operational costs, which varies
-                across countries based on the average server running time to
-                grab a slot.
-              </p>
-              <p>
-                We encourage you to value your chance, so that everybody gets a
-                fair shot at the slots. Also, fees are on us if you refund a
-                request. Therefore, price will be higher for returning users.
+                2. Collect at least price/2 likes and bookmarks (that's 10 each
+                if you paid $20).
               </p>
             </div>
+            <p>
+              Pricing is determined by the operational costs, which varies
+              across countries based on the average server running time to grab
+              a slot.
+            </p>
+            <p>
+              We encourage you to value your chance, so that everybody gets a
+              fair shot at the slots. Also, fees are on us if you refund a
+              request. Therefore, price will be higher for returning users.
+            </p>
           </div>
         </div>
         <h2 className="text-2xl font-bold" ref={ref}>
