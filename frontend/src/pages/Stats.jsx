b1f413c429bf6c31950dfcce3a56fe697f418682
Eric N <hannnie2@gmail.com>
2024-03-26 23:55:55 -0700

    updated redash links

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -5,9 +5,9 @@ export default function Stats() {
   const { country } = useParams();
   const links = {
     "The Americas":
-      "https://bass-harmless-singularly.ngrok-free.app/public/dashboards/qrJjgOHiBBhlxdJGhHWkOOcS4yCdRS8eQlzRJvVd?org_slug=default",
+      "https://redash.usvisa.lol/public/dashboards/qrJjgOHiBBhlxdJGhHWkOOcS4yCdRS8eQlzRJvVd?org_slug=default",
     Europe:
-      "https://bass-harmless-singularly.ngrok-free.app/public/dashboards/8f9ysadqeQHjPbfuOGl8j0SuE3QF7hBhMvoI8Gqx?org_slug=default",
+      "https://redash.usvisa.lol/public/dashboards/8f9ysadqeQHjPbfuOGl8j0SuE3QF7hBhMvoI8Gqx?org_slug=default",
   };
   return (
     <div className="flex h-screen w-full flex-col items-center gap-y-6 py-12">
